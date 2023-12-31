ae33cedc4393b8c2af311f2e92d8de9b12ca2b77
gabrielefabro <fabro.1934872@studenti.uniroma1.it>
2023-12-31 11:42:33 +0100

    things

--- Implementazione/Components/conditioner.cpp
+++ Implementazione/Components/conditioner.cpp
@@ -1,5 +1,65 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+
+class Device
+{
+private:
+    std::string nome;
+    bool state;
+    int id;
+
+public:
+    // Constructor
+    Device(std::string deviceName, int deviceId) : nome(deviceName), state(false), id(deviceId) {}
+
+    // Functions
+    void turnOn()
+    {
+        state = true;
+        std::cout << nome << " is turned on." << std::endl;
+    }
+
+    void turnOff()
+    {
+        state = false;
+        std::cout << nome << " is turned off." << std::endl;
+    }
+
+    // Getter functions
+    bool getState() const
+    {
+        return state;
+    }
+
+    std::string getNome() const
+    {
+        return nome;
+    }
+
+    void operateAtScheduledTimes(const std::chrono::system_clock::time_point &startTime, const std::chrono::duration<int> &duration)
+    {
+        auto endTime = startTime + duration;
+
+        while (true)
+        {
+            auto currentTime = std::chrono::system_clock::now();
+
+            if (currentTime > startTime && currentTime < endTime)
+            {
+                if (!state)
+                {
+                    turnOn();
+                }
+            }
+            else
+            {
+                turnOff();
+                break;
+            }
+        }
+    }
+};
 
 class Conditioner : public Device
 {
